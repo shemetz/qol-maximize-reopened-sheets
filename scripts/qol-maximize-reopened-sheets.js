9f4c3276ff3566ef48556f5617d3e3d1be3f0a49
itamar <itamarcu@gmail.com>
2021-12-26 16:17:46 +0200

    Fixed and refactored code further, should be more stable now

--- scripts/qol-maximize-reopened-sheets.js
+++ scripts/qol-maximize-reopened-sheets.js
@@ -1,7 +1,7 @@
 const MODULE_ID = 'qol-maximize-reopened-sheets'
 const currentlyOpenedSheetWindows = {}
 // special workaround for all the "new" objects created and immediately rendered
-const SPECIAL_SHEETS = [
+const SINGLE_INSTANCE_APPLICATION_SHEETS = [
   // in _onSettingsButton()
   'module-management',
   'world-config',
@@ -14,6 +14,8 @@ const SPECIAL_SHEETS = [
   'combat-tracker-config',
 ]
 
+// bad sheet windows that don't include a specific document ID.  we can't be compatible with them :(
+// see https://gitlab.com/foundrynet/foundryvtt/-/issues/6379
 const BAD_SHEETS = [
   'user-config',
   'folder-config',
@@ -27,47 +29,80 @@ const BAD_SHEETS = [
  * This is already what happens when you click things in the sidebar (see _onClickDocumentName) and
  * when you double-click a token (see _onClickLeft2). This module simply applies the same behavior in all
  * situations instead of only some situations.
- *
- * (overrides both DocumentSheet.render() and Application.render())
  */
-function renderWrapper (wrapped, ...args) {
+function documentSheetRenderWrapper (wrapped, force, options) {
   // Avoid wrapping if module is disabled through settings
   if (game.settings.get(MODULE_ID, 'disable-for-current-user')) {
-    return wrapped(...args)
+    return wrapped(force, options)
   }
-  const sheetId = this.id
-  if (BAD_SHEETS.some(badSheetId => badSheetId === sheetId)) {
-    // For some reason Foundry doesn't assign separate IDs to some config sheets, so only one window acn be opened.  probably a foundry bug?
-    return wrapped(...args)
+  if (!force) {
+    return wrapped(force, options)
   }
   // If the sheet is already rendered:
   if (this.rendered) {
-    this.bringToTop()
-    return this.maximize()
+    maximizeAndBringToTopIfNeeded(this)
   }
+  return wrapped(force, options)
+}
+
+/**
+ * When trying to render a sheet window that is already opened, the code will maximize it and bring
+ * it in front of other sheets.
+ *
+ * This is already what happens when you click things in the sidebar (see _onClickDocumentName) and
+ * when you double-click a token (see _onClickLeft2). This module simply applies the same behavior in all
+ * situations instead of only some situations.
+ *
+ * (overrides Application.render())
+ */
+function applicationRenderWrapper (wrapped, force, options) {
+  // Avoid wrapping if module is disabled through settings
+  if (game.settings.get(MODULE_ID, 'disable-for-current-user')) {
+    return wrapped(force, options)
+  }
+  if (!force) {
+    return wrapped(force, options)
+  }
+  const sheetId = this.id
+  const isSingleInstance = SINGLE_INSTANCE_APPLICATION_SHEETS.some(specialSheetId => specialSheetId === sheetId)
+  const isCompatibleApplicationSheet = !BAD_SHEETS.some(badSheetId => badSheetId === sheetId)
+  if (!isSingleInstance && !isCompatibleApplicationSheet) {
+    return wrapped(force, options)
+  }
+  if (this.rendered) {
+    maximizeAndBringToTopIfNeeded(this)
   // Special workaround check for some special settings sheets
-  const existingOpenSheet = currentlyOpenedSheetWindows[sheetId]
-  if (existingOpenSheet) {
-    // If an existing sheet is already rendered (as a different instance), reuse it and ignore the new one
-    if(existingOpenSheet.rendered) {
-      existingOpenSheet.bringToTop()
-      return existingOpenSheet.maximize()
-    } else {
-      // If we got here it means there's old invalid data in currentlyOpenedSheetWindows
-      // so we'll clean it up
-      for (const [id, sheet] of Object.entries(currentlyOpenedSheetWindows)) {
-        if (!sheet.rendered) {
-          delete currentlyOpenedSheetWindows[id]
+  } else if (isSingleInstance) {
+    const existingOpenSheet = currentlyOpenedSheetWindows[sheetId]
+    if (existingOpenSheet) {
+      // If an existing sheet is already rendered (as a different instance), reuse it and ignore the new one
+      if (existingOpenSheet.rendered) {
+        maximizeAndBringToTopIfNeeded(existingOpenSheet)
+        return
+      } else {
+        // If we got here it means there's old invalid data in currentlyOpenedSheetWindows
+        // so we'll clean it up
+        for (const [id, sheet] of Object.entries(currentlyOpenedSheetWindows)) {
+          if (!sheet.rendered) {
+            delete currentlyOpenedSheetWindows[id]
+          }
         }
       }
     }
-  }
-  // Otherwise render the sheet as normal
-  // (keeping it in memory if it needs a workaround later)
-  if (SPECIAL_SHEETS.some(specialSheetId => sheetId.startsWith(specialSheetId))) {
+    // Keep it in memory before rendering it;  next time we try to rerender it we might use this as the existing one
     currentlyOpenedSheetWindows[sheetId] = this
   }
-  return wrapped(...args)
+  // Render the sheet as normal
+  return wrapped(force, options)
+}
+
+const maximizeAndBringToTopIfNeeded = (sheet) => {
+  if (document.defaultView.getComputedStyle(sheet.element[0]).zIndex < _maxZ) {
+    sheet.bringToTop()
+  }
+  if (sheet._minimized) {
+    sheet.maximize()
+  }
 }
 
 Hooks.once('setup', function () {
@@ -75,13 +110,13 @@ Hooks.once('setup', function () {
   libWrapper.register(
     MODULE_ID,
     'DocumentSheet.prototype.render',
-    renderWrapper,
+    documentSheetRenderWrapper,
     libWrapper.MIXED,
   )
   libWrapper.register(
     MODULE_ID,
     'Application.prototype.render',
-    renderWrapper,
+    applicationRenderWrapper,
     libWrapper.MIXED,
   )
 })
